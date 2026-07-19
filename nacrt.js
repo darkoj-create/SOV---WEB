f18fe05c3a9ecf4249f92dd7534bc700243b3148
darkoj-create <darko.jeras@gmail.com>
2026-07-19 21:12:13 +0200

    Enable Nacrt renderer v2

--- nacrt.js
+++ nacrt.js
@@ -1,8 +1,10 @@
 /**
  * SOV Nacrt loader.
- * Loads the original generator, then applies the TopoDroid TDR compatibility parser.
+ * Loads the original generator, the TopoDroid compatibility parser,
+ * and the print-oriented renderer v2.
  */
 document.write(
-  '<script src="nacrt-core.js?v=20260719"></' + 'script>' +
-  '<script src="nacrt-tdr-fix.js?v=20260719"></' + 'script>'
+  '<script src="nacrt-core.js?v=6145al"></' + 'script>' +
+  '<script src="nacrt-tdr-fix.js?v=6145al"></' + 'script>' +
+  '<script src="nacrt-v2.js?v=6145al"></' + 'script>'
 );
